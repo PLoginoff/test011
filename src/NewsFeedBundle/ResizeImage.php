9e614ff4f97935435492d5ba8dc06e3253c3b858
Pavel Loginov <ploginoff@gmail.com>
2016-09-26 16:19:05 +0300

    Fix style

--- src/NewsFeedBundle/ResizeImage.php
+++ src/NewsFeedBundle/ResizeImage.php
@@ -1,18 +1,53 @@
 <?php
+/**
+ * ResizeImage
+ *
+ * PHP version 5.6
+ *
+ * @category Class
+ * @package  NewsFeedBundle
+ * @author   Pavel Loginov <ploginoff@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.example.com/
+ */
 
 namespace NewsFeedBundle;
 
 use Eventviva\ImageResize;
 
+/**
+ * ResizeImage
+ *
+ * @category Class
+ * @package  NewsFeedBundle
+ * @author   Pavel Loginov <ploginoff@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://www.example.com/
+ */
 class ResizeImage
 {
     protected $filename;
 
+    /**
+     * Construnct
+     *
+     * @param string $filename File
+     *
+     * @return null
+     */
     public function __construct($filename)
     {
         $this->filename = $filename;
     }
 
+    /**
+     * Resize
+     *
+     * @param int $width  Width
+     * @param int $height Height
+     * 
+     * @return boolena
+     */
     public function resize($width = 100, $height = 100)
     {
         $image = new ImageResize($this->filename);
